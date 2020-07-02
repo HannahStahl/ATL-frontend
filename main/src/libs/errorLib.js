8e65282336df9a6f36c21662604d2e98439aac35
Hannah Stahl <hls62@cornell.edu>
2020-07-02 11:27:03 -0500

    Forgot to commit some files

--- main/src/libs/errorLib.js
+++ main/src/libs/errorLib.js
@@ -1,26 +1,3 @@
-import * as Sentry from "@sentry/browser";
-
-const isLocal = process.env.NODE_ENV === "development";
-
-export function initSentry() {
-  if (isLocal) {
-    return;
-  }
-
-  Sentry.init({ dsn: "https://5f83aa2e21064e47bab8a1f308f940eb@sentry.io/5185720" });
-}
-
-export function logError(error, errorInfo = null) {
-  if (isLocal) {
-    return;
-  }
-
-  Sentry.withScope((scope) => {
-    errorInfo && scope.setExtras(errorInfo);
-    Sentry.captureException(error);
-  });
-}
-
 export function onError(error) {
   let errorInfo = {};
   let message = error.toString();
@@ -34,8 +11,6 @@ export function onError(error) {
   } else if (error.config && error.config.url) {
     errorInfo.url = error.config.url;
   }
-
-  logError(error, errorInfo);
 
   alert(message);
 }
