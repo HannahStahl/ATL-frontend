43085780ee022546d1239701a222bc483aafe150
Hannah Stahl <hls62@cornell.edu>
2020-07-17 10:49:39 -0500

    Fixed missing ATL link in navbar

--- src/App.js
+++ src/App.js
@@ -104,53 +104,51 @@ function App() {
             </Navbar.Brand>
             <Navbar.Toggle />
           </Navbar.Header>
-          <Navbar fluid>
-            <Nav pullRight>
-              {isAuthenticated ? (
-                profile && profile.userId ? (
-                  <>
-                    <NavDropdown
-                      title={(
-                        <p>
-                          {`${profile.firstName} ${profile.lastName}`}
-                          <i className="fas fa-user-circle" />
-                        </p>
-                      )}
-                      id="basic-nav-dropdown"
-                    >
-                      <MenuItem href="/profile">My Profile</MenuItem>
-                      {profile.isCaptain && profile.isAdmin && (
-                        <div className="line-header">CAPTAIN PAGES</div>
-                      )}
-                      {profile.isCaptain && <MenuItem href="/team-details">Team Details</MenuItem>}
-                      {profile.isCaptain && <MenuItem href="/team-roster">Team Roster</MenuItem>}
-                      {profile.isCaptain && <MenuItem href="/match-schedule">Match Schedule</MenuItem>}
-                      {profile.isCaptain && profile.isAdmin && (
-                        <div className="line-header">ADMIN PAGES</div>
-                      )}
-                      {profile.isAdmin && <MenuItem href="/seasons">Seasons</MenuItem>}
-                      {profile.isAdmin && <MenuItem href="/season-calendars">Season Calendars</MenuItem>}
-                      {profile.isAdmin && <MenuItem href="/teams">Teams</MenuItem>}
-                      {profile.isAdmin && <MenuItem href="/players">Players</MenuItem>}
-                      {profile.isAdmin && <MenuItem href="/court-locations">Court Locations</MenuItem>}
-                      {profile.isAdmin && <MenuItem href="/associations">Associations</MenuItem>}
-                      {profile.isAdmin && <MenuItem href="/match-schedules">Match Schedules</MenuItem>}
-                      {profile.isAdmin && <MenuItem href="/divisions">Divisions</MenuItem>}
-                      {profile.isAdmin && <MenuItem href="/users">Users</MenuItem>}
-                      <MenuItem onClick={handleLogout}>Log out</MenuItem>
-                    </NavDropdown>
-                  </>
-                ) : <></>
-              ) : (
-                <LinkContainer to="/login">
-                  <NavItem>
-                    Login
-                    <i className="fas fa-user-circle" />
-                  </NavItem>
-                </LinkContainer>
-              )}
-            </Nav>
-          </Navbar>
+          <Nav pullRight>
+            {isAuthenticated ? (
+              profile && profile.userId ? (
+                <>
+                  <NavDropdown
+                    title={(
+                      <p>
+                        {`${profile.firstName} ${profile.lastName}`}
+                        <i className="fas fa-user-circle" />
+                      </p>
+                    )}
+                    id="basic-nav-dropdown"
+                  >
+                    <MenuItem href="/profile">My Profile</MenuItem>
+                    {profile.isCaptain && profile.isAdmin && (
+                      <div className="line-header">CAPTAIN PAGES</div>
+                    )}
+                    {profile.isCaptain && <MenuItem href="/team-details">Team Details</MenuItem>}
+                    {profile.isCaptain && <MenuItem href="/team-roster">Team Roster</MenuItem>}
+                    {profile.isCaptain && <MenuItem href="/match-schedule">Match Schedule</MenuItem>}
+                    {profile.isCaptain && profile.isAdmin && (
+                      <div className="line-header">ADMIN PAGES</div>
+                    )}
+                    {profile.isAdmin && <MenuItem href="/seasons">Seasons</MenuItem>}
+                    {profile.isAdmin && <MenuItem href="/season-calendars">Season Calendars</MenuItem>}
+                    {profile.isAdmin && <MenuItem href="/teams">Teams</MenuItem>}
+                    {profile.isAdmin && <MenuItem href="/players">Players</MenuItem>}
+                    {profile.isAdmin && <MenuItem href="/court-locations">Court Locations</MenuItem>}
+                    {profile.isAdmin && <MenuItem href="/associations">Associations</MenuItem>}
+                    {profile.isAdmin && <MenuItem href="/match-schedules">Match Schedules</MenuItem>}
+                    {profile.isAdmin && <MenuItem href="/divisions">Divisions</MenuItem>}
+                    {profile.isAdmin && <MenuItem href="/users">Users</MenuItem>}
+                    <MenuItem onClick={handleLogout}>Log out</MenuItem>
+                  </NavDropdown>
+                </>
+              ) : <></>
+            ) : (
+              <LinkContainer to="/login">
+                <NavItem>
+                  Login
+                  <i className="fas fa-user-circle" />
+                </NavItem>
+              </LinkContainer>
+            )}
+          </Nav>
         </Navbar>
         <ErrorBoundary>
           <AppContext.Provider value={{
