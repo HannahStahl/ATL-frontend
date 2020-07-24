c91635fb2132a96b289985d87028d1e39658fc95
Hannah Stahl <hls62@cornell.edu>
2020-07-24 10:57:28 -0500

    Fixed mobile styling of Match Schedules page

--- src/DivisionSchedules.js
+++ src/DivisionSchedules.js
@@ -42,69 +42,77 @@ export default function DivisionSchedules() {
         </FormControl>
       )}
       <div className="centered-content">
-        {teams.length > 0 && (
-          <Table bordered>
-            <thead>
-              <tr>
-                <th>Team Name</th>
-                <th>Captain/Co-Captain</th>
-              </tr>
-            </thead>
-            <tbody>
-              {teams.map((team) => {
-                const { captainId, cocaptainId } = team;
-                const captain = captainId && allCaptains.find((captainInList) => captainInList.userId === captainId);
-                const cocaptain = cocaptainId && allCaptains.find((captainInList) => captainInList.userId === cocaptainId);
-                return (
-                  <tr key={team.teamId}>
-                    <td>{team.teamName}</td>
-                    <td>
-                      {captain ? `${captain.firstName || ""} ${captain.lastName || ""}` : ""}
-                      {captain && cocaptain ? ", " : ""}
-                      {cocaptain ? `${cocaptain.firstName || ""} ${cocaptain.lastName || ""}` : ""}
-                    </td>
+        <div className="centered-content-inner">
+          {teams.length > 0 && (
+            <div className="table-container">
+              <Table bordered>
+                <thead>
+                  <tr>
+                    <th>Team Name</th>
+                    <th>Captain/Co-Captain</th>
                   </tr>
-                );
-              })}
-            </tbody>
-          </Table>
-        )}
+                </thead>
+                <tbody>
+                  {teams.map((team) => {
+                    const { captainId, cocaptainId } = team;
+                    const captain = captainId && allCaptains.find((captainInList) => captainInList.userId === captainId);
+                    const cocaptain = cocaptainId && allCaptains.find((captainInList) => captainInList.userId === cocaptainId);
+                    return (
+                      <tr key={team.teamId}>
+                        <td>{team.teamName}</td>
+                        <td>
+                          {captain ? `${captain.firstName || ""} ${captain.lastName || ""}` : ""}
+                          {captain && cocaptain ? ", " : ""}
+                          {cocaptain ? `${cocaptain.firstName || ""} ${cocaptain.lastName || ""}` : ""}
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </Table>
+            </div>
+          )}
+        </div>
       </div>
       <div className="centered-content">
         <p>The home team must supply 4 cans of USTA-approved tennis balls.</p>
       </div>
       <div className="centered-content">
-      {schedule.length > 0 && (
-        <Table bordered>
-          <thead>
-            <tr>
-              <th>Week</th>
-              <th>Date</th>
-              <th>Home</th>
-              <th>Visitor</th>
-              <th>Location</th>
-              <th>Time</th>
-            </tr>
-          </thead>
-          <tbody>
-            {schedule.map((match) => {
-              const home = match.homeTeamId && allTeams.find((team) => team.teamId === match.homeTeamId);
-              const visitor = match.visitorTeamId && allTeams.find((team) => team.teamId === match.visitorTeamId);
-              const location = locations.find((locationInList) => locationInList.locationId === match.locationId);
-              return (
-                <tr key={match.matchId}>
-                  <td>{match.weekNumber || ""}</td>
-                  <td>{match.matchDate ? moment(match.matchDate).format("M/D/YYYY") : ""}</td>
-                  <td>{home ? home.teamName : ""}</td>
-                  <td>{visitor ? visitor.teamName : ""}</td>
-                  <td>{location ? location.locationName : ""}</td>
-                  <td>{match.startTime}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </Table>
-      )}
+        <div className="centered-content-inner">
+          {schedule.length > 0 && (
+            <div className="table-container">
+              <Table bordered>
+                <thead>
+                  <tr>
+                    <th>Week</th>
+                    <th>Date</th>
+                    <th>Home</th>
+                    <th>Visitor</th>
+                    <th>Location</th>
+                    <th>Time</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {schedule.map((match) => {
+                    const home = match.homeTeamId && allTeams.find((team) => team.teamId === match.homeTeamId);
+                    const visitor = match.visitorTeamId && allTeams.find((team) => team.teamId === match.visitorTeamId);
+                    const location = locations.find((locationInList) => locationInList.locationId === match.locationId);
+                    return (
+                      <tr key={match.matchId}>
+                        <td>{match.weekNumber || ""}</td>
+                        <td>{match.matchDate ? moment(match.matchDate).format("M/D/YYYY") : ""}</td>
+                        <td>{home ? home.teamName : ""}</td>
+                        <td>{visitor ? visitor.teamName : ""}</td>
+                        <td>{location ? location.locationName : ""}</td>
+                        <td>{match.startTime}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </Table>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
