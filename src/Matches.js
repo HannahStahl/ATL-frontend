55c3f8d6c24a6b5cbc5b57f4f3ade33f8c9ead3b
Hannah Stahl <hls62@cornell.edu>
2020-08-07 14:23:44 -0400

    Added players to captains' Match Schedule table and form

--- src/Matches.js
+++ src/Matches.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import moment from "moment";
 import { API } from "aws-amplify";
 import Table from "./Table";
@@ -6,6 +6,29 @@ import { useAppContext } from "./libs/contextLib";
 
 export default ({ team }) => {
   const { allMatches, setAllMatches, locations, allTeams, loadingData } = useAppContext();
+  const [allPlayers, setAllPlayers] = useState([]);
+
+  useEffect(() => {
+    API.get("atl-backend", "list/player").then(setAllPlayers);
+  }, []);
+
+  const playerColumn = (label, home) => ({
+    label,
+    type: "dropdown",
+    joiningTable: allPlayers,
+    joiningTableFilter: {
+      key: home ? "homeTeamId" : "visitorTeamId",
+      joiningTableKey: "teamId"
+    },
+    joiningTableKey: "playerId",
+    joiningTableFieldNames: ["firstName", "lastName"]
+  });
+
+  const doublesColumn = (label, home, children) => ({
+    label,
+    children: children.map((child) => ({ key: child.key, ...playerColumn(child.label, home) })),
+    childrenJoiner: ', '
+  });
 
   const columns = {
     weekNumber: { label: "Week", readOnly: true },
@@ -37,6 +60,26 @@ export default ({ team }) => {
       joiningTableFieldNames: ["teamName"],
       readOnly: true
     }, // TODO change these to Home boolean and Opponent name
+    singles1HomePlayerId: playerColumn("S1 Home Player", true),
+    singles1VisitorPlayerId: playerColumn("S1 Visitor Player", false),
+    singles2HomePlayerId: playerColumn("S2 Home Player", true),
+    singles2VisitorPlayerId: playerColumn("S2 Visitor Player", false),
+    doubles1HomePlayers: doublesColumn("D1 Home Players", true, [
+      { key: "doubles1HomePlayer1Id", label: "D1 Home Players" },
+      { key: "doubles1HomePlayer2Id" }
+    ]),
+    doubles1VisitorPlayers: doublesColumn("D1 Visitor Players", false, [
+      { key: "doubles1VisitorPlayer1Id", label: "D1 Visitor Players" },
+      { key: "doubles1VisitorPlayer2Id" }
+    ]),
+    doubles2HomePlayers: doublesColumn("D2 Home Players", true, [
+      { key: "doubles2HomePlayer1Id", label: "D2 Home Players" },
+      { key: "doubles2HomePlayer2Id" }
+    ]),
+    doubles2VisitorPlayers: doublesColumn("D2 Visitor Players", false, [
+      { key: "doubles2VisitorPlayer1Id", label: "D2 Visitor Players" },
+      { key: "doubles2VisitorPlayer2Id" }
+    ]),
     singles1Score: { label: "S1 Score", type: "text" },
     singles2Score: { label: "S2 Score", type: "text" },
     doubles1Score: { label: "D1 Score", type: "text" },
