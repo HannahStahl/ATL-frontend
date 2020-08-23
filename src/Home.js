414a2ed1bb3bab4d6650be4d52b73c25354d10b5
Hannah Stahl <hls62@cornell.edu>
2020-08-23 15:48:17 -0500

    Gave max width to home page cards so they never expand to 3 columns

--- src/Home.js
+++ src/Home.js
@@ -64,125 +64,127 @@ export default function Home() {
           The Austin Tennis League (ATL) offers all levels of singles and doubles, in both Fall and Spring leagues.
           Matches are held on Saturdays throughout the Austin area, in addition to other ad hoc tennis activities.
         </h4>
-        <Masonry
-          className="masonry-layout"
-          options={{ isFitWidth: true }}
-        >
-          <div className="home-section-2-card">
-            <h3>Sign Up to Play</h3>
-            <p>
-              New to Austin or ATL? <a href="/player-signup">Sign up to participate</a> in the league.
-            </p>
-          </div>
-          <div className="home-section-2-card leader-board-card">
-            <h3>Leader Board</h3>
-            <div className="table-container">
-              <Table hover className="home-page-leader-board interactive-table">
-                <thead>
-                  <tr>
-                    <th>Division</th>
-                    <th>1st</th>
-                    <th>2nd</th>
-                    <th>3rd</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {divisions.map((division) => {
-                    const teams = getOrderedTeamsInDivision(allTeams, standings, division.divisionId);
-                    const standingsUrl = `/standings?divisionId=${division.divisionId}`;
+        <div className="masonry-container centered-content">
+          <Masonry
+            className="masonry-layout"
+            options={{ isFitWidth: true }}
+          >
+            <div className="home-section-2-card">
+              <h3>Sign Up to Play</h3>
+              <p>
+                New to Austin or ATL? <a href="/player-signup">Sign up to participate</a> in the league.
+              </p>
+            </div>
+            <div className="home-section-2-card leader-board-card">
+              <h3>Leader Board</h3>
+              <div className="table-container">
+                <Table hover className="home-page-leader-board interactive-table">
+                  <thead>
+                    <tr>
+                      <th>Division</th>
+                      <th>1st</th>
+                      <th>2nd</th>
+                      <th>3rd</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {divisions.map((division) => {
+                      const teams = getOrderedTeamsInDivision(allTeams, standings, division.divisionId);
+                      const standingsUrl = `/standings?divisionId=${division.divisionId}`;
+                      return (
+                        <tr key={division.divisionId} onClick={() => window.location.href = standingsUrl}>
+                          <td>{division.divisionNumber}</td>
+                          {[0, 1, 2].map((index) => <td key={index}>{teams[index]}</td>)}
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </Table>
+              </div>
+            </div>
+            <div className="home-section-2-card">
+              <h3>Captain Registration</h3>
+              <p>
+                New captains should email Maggie Yanez at <a href="mailto:myanez@pharrtennis.com">myanez@pharrtennis.com</a> or call Maggie at (512) 477-7773 to get set up.
+              </p>
+            </div>
+            <div className="home-section-2-card calendar-card">
+              {season.seasonName && (
+                <>
+                  <h3>{`${season.seasonName || ""} Key Dates`}</h3>
+                  <table className="key-dates home-page-calendar">
+                    <tbody>
+                      {seasonEvents.map((event) => (
+                        <tr key={event.eventId}>
+                          <td className="key-date bold">
+                            {event.endDate ? (
+                              `${moment(event.startDate).format("MMM. D")} - ${moment(event.endDate).format("MMM. D")}`
+                            ) : (
+                              moment(event.startDate).format("MMM. D")
+                            )}
+                          </td>
+                          <td className="key-date key-event">{event.eventName || ""}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                  <a href="/calendar">View full calendar</a>
+                </>
+              )}
+            </div>
+            <div className="home-section-2-card">
+              <h3>New Player Program</h3>
+              <p>
+                In addition to league play, ATL offers other tennis activities to new players.
+                "New" includes experienced players who are new to Austin or those returning to tennis after a break.
+                For more info, contact Roger Vallejo at <a href="mailto:roger_vallejo@hotmail.com">roger_vallejo@hotmail.com</a>.
+              </p>
+            </div>
+            <div className="home-section-2-card weather-card">
+              <h2>Today in Austin</h2>
+              {weather.length > 0 && (
+                <div className="weather">
+                  {weatherHours.map((index) => {
+                    const hour = weather[index];
                     return (
-                      <tr key={division.divisionId} onClick={() => window.location.href = standingsUrl}>
-                        <td>{division.divisionNumber}</td>
-                        {[0, 1, 2].map((index) => <td key={index}>{teams[index]}</td>)}
-                      </tr>
+                      <div key={index} className="weather-hour">
+                        <img
+                          src={`${config.weatherIconBaseUrl}/${hour.weather[0].icon}@2x.png`}
+                          alt={hour.weather[0].description}
+                        />
+                        <h4>{moment.unix(hour.dt).format("hA")}</h4>
+                        <h3>{`${hour.temp.toFixed(0)}°F`}</h3>
+                        <p>{hour.weather[0].main}</p>
+                        <p>
+                          <i className="raindrop fas fa-tint" />
+                          {`${hour.pop.toFixed(0)}%`}
+                        </p>
+                      </div>
                     );
                   })}
-                </tbody>
-              </Table>
+                </div>
+              )}
+              <a
+                className="link-below-button"
+                href="https://www.accuweather.com/en/us/austin/78701/hourly-weather-forecast/351193"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                View full forecast
+                <i className="weather-external-link fas fa-external-link-alt" />
+              </a>
+              <a
+                className="link-below-button"
+                href="https://www.kvue.com/article/weather/allergy-forecast/allergy-report/269-44055429"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                View allergy report
+                <i className="weather-external-link fas fa-external-link-alt" />
+              </a>
             </div>
-          </div>
-          <div className="home-section-2-card">
-            <h3>Captain Registration</h3>
-            <p>
-              New captains should email Maggie Yanez at <a href="mailto:myanez@pharrtennis.com">myanez@pharrtennis.com</a> or call Maggie at (512) 477-7773 to get set up.
-            </p>
-          </div>
-          <div className="home-section-2-card calendar-card">
-            {season.seasonName && (
-              <>
-                <h3>{`${season.seasonName || ""} Key Dates`}</h3>
-                <table className="key-dates home-page-calendar">
-                  <tbody>
-                    {seasonEvents.map((event) => (
-                      <tr key={event.eventId}>
-                        <td className="key-date bold">
-                          {event.endDate ? (
-                            `${moment(event.startDate).format("MMM. D")} - ${moment(event.endDate).format("MMM. D")}`
-                          ) : (
-                            moment(event.startDate).format("MMM. D")
-                          )}
-                        </td>
-                        <td className="key-date key-event">{event.eventName || ""}</td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-                <a href="/calendar">View full calendar</a>
-              </>
-            )}
-          </div>
-          <div className="home-section-2-card">
-            <h3>New Player Program</h3>
-            <p>
-              In addition to league play, ATL offers other tennis activities to new players.
-              "New" includes experienced players who are new to Austin or those returning to tennis after a break.
-              For more info, contact Roger Vallejo at <a href="mailto:roger_vallejo@hotmail.com">roger_vallejo@hotmail.com</a>.
-            </p>
-          </div>
-          <div className="home-section-2-card weather-card">
-            <h2>Today in Austin</h2>
-            {weather.length > 0 && (
-              <div className="weather">
-                {weatherHours.map((index) => {
-                  const hour = weather[index];
-                  return (
-                    <div key={index} className="weather-hour">
-                      <img
-                        src={`${config.weatherIconBaseUrl}/${hour.weather[0].icon}@2x.png`}
-                        alt={hour.weather[0].description}
-                      />
-                      <h4>{moment.unix(hour.dt).format("hA")}</h4>
-                      <h3>{`${hour.temp.toFixed(0)}°F`}</h3>
-                      <p>{hour.weather[0].main}</p>
-                      <p>
-                        <i className="raindrop fas fa-tint" />
-                        {`${hour.pop.toFixed(0)}%`}
-                      </p>
-                    </div>
-                  );
-                })}
-              </div>
-            )}
-            <a
-              className="link-below-button"
-              href="https://www.accuweather.com/en/us/austin/78701/hourly-weather-forecast/351193"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              View full forecast
-              <i className="weather-external-link fas fa-external-link-alt" />
-            </a>
-            <a
-              className="link-below-button"
-              href="https://www.kvue.com/article/weather/allergy-forecast/allergy-report/269-44055429"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              View allergy report
-              <i className="weather-external-link fas fa-external-link-alt" />
-            </a>
-          </div>
-        </Masonry>
+          </Masonry>
+        </div>
       </div>
     </div>
   );
