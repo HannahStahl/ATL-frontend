562044bd50e41e55160bf1fad3262fffb3371266
Hannah Stahl <hls62@cornell.edu>
2022-12-20 22:31:08 -0500

    Update fee

--- src/Payment.js
+++ src/Payment.js
@@ -13,16 +13,16 @@ export default function Payment() {
       <div className="centered-content">
         <div>
           <h3>Option 1: Pay with Zelle</h3>
-          <p className="no-margin-bottom">ATL now accepts Zelle. If your bank allows Zelle, send payment for <b>$200</b> to atl@atltennis.org.</p>
+          <p className="no-margin-bottom">ATL now accepts Zelle. If your bank allows Zelle, send payment for <b>$225</b> to atl@atltennis.org.</p>
           <p className="no-margin-bottom">Currently there is no fee on our side.</p>
           <p>Some banks may require you to use the Zelle mobile app. Some banks require you to either login online, or use their mobile app.</p>
           <h3>Option 2: Pay by check</h3>
-          <p className="no-margin-bottom">Please write a check for <b>$200</b>.</p>
+          <p className="no-margin-bottom">Please write a check for <b>$225</b>.</p>
           <p>All checks must be made out to <b>Austin Tennis League</b>.</p>
           <p className="no-margin-bottom">Checks can be dropped off at Pharr Tennis Center or mailed to the following address:</p>
           <p>Austin Tennis League<br />PO Box 300035<br />Austin, TX 78703</p>
           <h3>Option 3: Pay with PayPal</h3>
-          <p className="no-margin-bottom">Please pay <b>$207.74</b> via PayPal below.</p>
+          <p className="no-margin-bottom">Please pay <b>$233.64</b> via PayPal below.</p>
           <p className="no-margin-bottom">Note that you do not have to have a PayPal account to use PayPal. All you need is a credit card.</p>
           <p>The current season is {seasonName}. If paying for the previous season, please note that in Comments.</p>
           <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
@@ -30,7 +30,7 @@ export default function Payment() {
             <input type="hidden" name="business" value="atl@atltennis.org" />
             <input type="hidden" name="lc" value="US" />
             <input type="hidden" name="item_name" value="Fee Payment" />
-            <input type="hidden" name="amount" value="206.28" />
+            <input type="hidden" name="amount" value="233.64" />
             <input type="hidden" name="currency_code" value="USD" />
             <input type="hidden" name="button_subtype" value="services" />
             <input type="hidden" name="no_note" value="0" />
