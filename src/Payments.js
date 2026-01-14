72cfa5e6706edea08b7b629dc9bc8be534b99381
Hannah Stahl <hls62@cornell.edu>
2026-01-13 22:02:25 -0500

    Show all teams on payments page, even those that have not paid yet

--- src/Payments.js
+++ src/Payments.js
@@ -1,26 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { PageHeader } from "react-bootstrap";
 import { API } from "aws-amplify";
 import Table from "./Table";
 import { useAppContext } from "./libs/contextLib";
 
-
-
 export default () => {
   const { payments, setPayments, allTeams, loadingData } = useAppContext();
+
+  const [teamsWithPayments, setTeamsWithPayments] = useState([]);
+
+  useEffect(() => {
+    const activeTeams = allTeams.filter((team) => team.isActive);
+    setTeamsWithPayments(activeTeams.map((team) => {
+      const teamPayment = payments.find((payment) => payment.teamId === team.teamId);
+      return { ...team, ...(teamPayment || {})};
+    }));
+  }, [allTeams, payments]);
+
   const columns = {
     teamId: {
       label: "Team",
-      type: "dropdown",
-      required: true,
-      joiningTable: allTeams.filter((team) => team.isActive && team.teamName !== "Bye"),
+      joiningTable: allTeams,
       joiningTableKey: "teamId",
       joiningTableFieldNames: ["teamName"],
+      readOnly: true,
     },
-    payerName: { label: "Captain or Payer", type: "text", required: true },
-    amountPaid: { label: "Amount Paid", type: "text", required: true },
-    paymentMethod: { label: "Method", type: "text", required: true },
+    payerName: { label: "Captain or Payer", type: "text" },
+    amountPaid: { label: "Amount Paid", type: "text" },
+    paymentMethod: { label: "Method", type: "text" },
   };
+
+  const getPayments = () => API.get("atl-backend", "list/payment");
+
+  const createOrUpdatePayment = async (paymentId, body) => {
+    if (paymentId) {
+      await API.put("atl-backend", `update/payment/${paymentId}`, { body });
+    } else {
+      await API.post("atl-backend", "create/payment", { body });
+    }
+    const updatedPayments = await getPayments();
+    setPayments([...updatedPayments]);
+  }
 
   return (
     <div className="container">
@@ -28,12 +48,13 @@ export default () => {
       {loadingData ? <p className="centered-text">Loading...</p> : (
         <Table
           columns={columns}
-          rows={payments}
-          filterRows={(list) => list.filter((row) => row.amountPaid)}
+          rows={teamsWithPayments}
           setRows={setPayments}
-          getRows={() => API.get("atl-backend", "list/payment")}
           itemType="payment"
           API={API}
+          createDisabled
+          removeDisabled
+          customEditFunction={createOrUpdatePayment}
         />
       )}
     </div>
